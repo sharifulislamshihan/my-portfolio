a01e359c01fd82d13e5d15e7d2e8cbbf637f5648
unknown <sharifulislamshihan@gmail.com>
2024-03-10 02:59:03 +0600

    services added

--- src/component/Services/Services.jsx
+++ src/component/Services/Services.jsx
@@ -1,38 +1,126 @@
-
+import { BiBug, BiCodeAlt, BiCodeCurly, BiCog } from "react-icons/bi";
 const Services = () => {
     return (
         <div>
             <h3 className="text-white text-5xl font-paragraph font-bold text-center">Service Provide For My Clients.</h3>
 
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-5 lg:mx-16 xl:mx-40 my-40">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-10 mx-12 lg:mx-20 xl:mx-32 my-40">
 
                 <div className="card shadow-xl bg-[#222222]">
-                    <img className="w-20 h-20" src="https://i.ibb.co/QvCnnv7/mern.png" alt="MERN STACK" />
+                    <BiCodeAlt className="text-white text-6xl mx-auto mt-12 mb-5 font-semibold" />
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                       
+                        <h2 className="card-title text-2xl text-[#e6e3dc] font-paragraph font-medium mt-3 mb-10 ">WEB DEVELOPMENT</h2>
+                        <div className="text-md font-heading">
+                            <ul className="list-none text-white opacity-50 space-y-2">
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> HTML / CSS
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> TAILWIND / BOOTSTRAP
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> JAVASCRIPT
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> RESPONSIVE DESIGN
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> REACT JS
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> NODE JS
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> EXPRESS JS
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> MONGODB
+                                </li>
+
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
 
                 <div className="card shadow-xl bg-[#222222]">
-                    <img className="w-20 h-20" src="https://i.ibb.co/QvCnnv7/mern.png" alt="Frontend" />
+                    <BiCog className="text-white text-6xl mx-auto mt-12 mb-5 font-semibold" />
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <h2 className="card-title text-2xl text-[#e6e3dc] font-paragraph font-medium mt-3 mb-10 ">CUSTOM APPLICATION</h2>
+                        <div className="text-md font-heading">
+                            <ul className="list-none text-white opacity-50 space-y-2">
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> CLIENT DEMAND WEBSITE
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> REDESIGN WEBSITE
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> API DEVELOPMENT and INTEGRATION
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> RESPONSIVE DESIGN
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
 
                 <div className="card shadow-xl bg-[#222222]">
-                    <img className="w-20 h-20" src="https://i.ibb.co/QvCnnv7/mern.png" alt="React" />
+                    <BiCodeCurly className="text-white text-6xl mx-auto mt-12 mb-5 font-semibold" />
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
+                        <h2 className="card-title text-2xl text-[#e6e3dc] font-paragraph font-medium mt-3 mb-10 ">FRONTEND DEVELOPMENT</h2>
+                        <div className="text-md font-heading">
+                            <ul className="list-none text-white opacity-50 space-y-2">
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> FIGMA TO HTML
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> FIGMA TO REACT
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> RESPONSIVE DESIGN
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> REACT JS
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> ANIMATION
+                                </li>
+                                <li className="flex items-center">
+                                    <span className="mx-5">»</span> PIXEL PERFECT DESIGN
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="card shadow-xl bg-[#222222]">
+                    <BiBug className="text-white text-6xl mx-auto mt-12 mb-5 font-semibold" />
+                    <div className="card-body">
+                        <h2 className="card-title text-2xl text-[#e6e3dc] font-paragraph font-medium mt-3 mb-10 ">TESTING & DEBUGGING</h2>
+                        <div className="text-md font-heading">
+                            <ul className="list-none text-white opacity-50 space-y-2">
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> WEB TESTING
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> BUG FIXING
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> FUNCTIONAL TESTING
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> ERROR HANDLING
+                                </li>
+                                <li className="flex items-center pb-3">
+                                    <span className="mx-5">»</span> IMPROVEMENT
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
